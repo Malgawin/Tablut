e068a16636c7506fc5c6aa21b1371bef94b49366
Malgawin <101182923+Malgawin@users.noreply.github.com>
2022-12-27 21:12:11 +0100

    Update mange.h

--- mange.h
+++ mange.h
@@ -1,17 +1,8 @@
 #ifndef TABLUT_MANGE_H
 #define TABLUT_MANGE_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-
-#define PION_BLANC 10
-#define ROI 11
-#define NOIR 12
-#define BLANC 13
-#define FORTERESSE 14
-#define CASE_VIDE 15
-#define PIEGE 16
+#include "Fonction.h"
 
 int type_case(int x, int y, int taille_ligne, int* plateau);
 void autour(int x, int y, int taille_ligne, int* plateau, int* pion_autour);
